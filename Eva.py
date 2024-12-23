d5bd5e4c526e7c41b43e1c6be4d1153a2516c211
Shiva Kumar <143744774+Shivakumar3542@users.noreply.github.com>
2024-12-23 19:41:41 +0530

    Update Eva.py

--- Eva.py
+++ Eva.py
@@ -18,15 +18,6 @@ chatbot = llm.start_chat(history=[])
 # Set up page layout with title
 st.set_page_config(page_title="Smartbot Assistant", page_icon="🤖", layout="wide")
 
-# # Title Section with styling
-# st.markdown("""
-#     <h1 style="font-family: 'Arial', sans-serif; color: #2E8B57; text-align: center;">
-#         Eva - Your Smart Companion
-#     </h1>
-# """, unsafe_allow_html=True)
-
-# Display AI greeting at the top
-# st.chat_message("ai").write("Hi! I’m Ellie, your smart assistant. How can I assist you today?")
 st.image(image)
 
 # Sidebar - Chat history
@@ -37,16 +28,16 @@ with st.sidebar:
             st.write(f"**{i + 1}. {role.capitalize()}**: {text}")
 
 # Chat message input field
-human_prompt = st.chat_input("Ask me anything...")
+User_prompt = st.chat_input("Ask me anything...")
 
 # Process user input and generate AI response
-if human_prompt:
+if User_prompt:
     # Append user message to chat history
-    st.session_state.chat_history.append(("human", human_prompt))
-    st.chat_message("human").write(human_prompt)
+    st.session_state.chat_history.append(("User", User_prompt))
+    st.chat_message("User").write(User_prompt)
 
     # Generate AI response
-    response = chatbot.send_message(human_prompt)
+    response = chatbot.send_message(User_prompt)
     ai_response = response.text
 
     # Append AI message to chat history
